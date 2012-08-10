155a6c00e07c6bb13a1177b6183c63353dd1aa00
rmzelle <rintze.zelle@gmail.com>
2012-08-09 21:48:40 -0400

    Forgot to build.

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,style:0,welcom:0,citat:0,modul:0,search:0,indic:0,content:0,tabl:0,document:0,page:0,languag:0},objtypes:{},titles:["Welcome to Citation Style Language&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{index:0,style:0,welcom:0,citat:0,modul:0,search:0,indic:0,content:0,tabl:0,test:0,document:0,page:0,languag:0},objtypes:{},titles:["Welcome to Citation Style Language&#8217;s documentation!"],objnames:{},filenames:["index"]})
